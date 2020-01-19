6f341d4fea08da264e407025d34a95cee1ffad4f
Steve Holmes <stephen.holmes2@gmail.com>
2020-01-18 18:11:52 -0800

    Add auto-hiding loading screen.

---
+++ Assets/Scripts/AutoHide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoHide : MonoBehaviour {
+
+    public float HideDelay;
+    public float HideAnimDuration;
+    private Animator anim;
+    
+    private void Start() {
+        anim = GetComponent<Animator>();
+        StartCoroutine(WaitForDelay());
+    }
+
+    private IEnumerator WaitForDelay() {
+        yield return new WaitForSeconds(HideDelay);
+        anim.Play("LoadingScreenAnim");
+        StartCoroutine(WaitForAnim());
+    }
+    
+    private IEnumerator WaitForAnim() {
+        yield return new WaitForSeconds(HideAnimDuration);
+        gameObject.SetActive(false);
+    }
+}
