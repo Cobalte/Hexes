8165398c7f8d258e8d1a644f478be0213e2916b3
jdeegz <jdeegz@gmail.com>
2020-01-20 10:45:00 -0800

    *Scaling and anim changes for the options menu

--- Assets/Scripts/OptionsMenuController.cs
+++ Assets/Scripts/OptionsMenuController.cs
@@ -14,6 +14,7 @@ public class OptionsMenuController : MonoBehaviour
         if (optionsMenuObj != null)
         {
             optionsMenuAnimator.SetBool("Status", false);
+            optionsMenuObj.SetActive(false);
         }
     }
 
@@ -25,6 +26,7 @@ public class OptionsMenuController : MonoBehaviour
 
     public void ToggleOptionsMenu()
     {
+        optionsMenuObj.SetActive(true);
         optionsMenuAnimator.SetBool("Status", !optionsMenuAnimator.GetBool("Status"));
     }
     
